e91dd101ce4af6a32f9492fa4aaaaf762a873ca6
RabiatuMohammed <rabiatyyusuf71@gmail.com>
2025-01-11 17:00:47 +0000

    Updated backend configuration in providers.tf

--- providers.tf
+++ providers.tf
@@ -22,7 +22,7 @@ terraform {
   }
   backend "azurerm" {
    resource_group_name = "rg-terraform-state"
-   storage_account_name = "stterraformstate"
+   storage_account_name = "sttfstaterabmoh"
    container_name = "tfstate"
    key = "security-platform.tfstate"
   }
